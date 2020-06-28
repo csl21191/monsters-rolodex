006a47f93bc88b0a1d65c5fbcb62028895935d48
Chandershekhar Luhaniwal <csl21191@gmail.com>
2020-06-28 20:22:25 +0530

    Updates

---
+++ precache-manifest.9c0b63d628b614bbae73287cafa504c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5557d95830ff10a0792fc8423f88eabb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "df40fa59c89bb7b7210e",
+    "url": "/monsters-rolodex/static/css/main.9da82fd5.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df40fa59c89bb7b7210e",
+    "url": "/monsters-rolodex/static/js/main.8fdf24f0.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
